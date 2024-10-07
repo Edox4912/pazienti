b70312b374d665f43ad9990a48a18467df4e6fda
loliknight000@gmail.com <gutsman.maksym.studente@itispaleocapa.it>
2024-10-07 18:12:56 +0200

    AYO

--- Class1.cs
+++ Class1.cs
@@ -6,8 +6,36 @@ using System.Threading;
 using System.Threading.Tasks;
 using pazienti;
 
-namespace tombola
+namespace Pazienti_biglietto
 {
+    public class Pazienti
+    {
+        static bool[] numeri = new bool[91];
+        static Random r = new Random();
+        static int estratti = 0;
+        public void Estrattore(object lista)
+        {
+            int n;
+            do
+            {
+                n = r.Next(1, 90);
+                if (!numeri[n])
+                {
+                    dynamic losta = lista;
+                    losta.Items.Add($"{n}");
+                    numeri[n] = true;
+                    ++estratti;
+                    Thread.Sleep(1000);
+                }
+            }
+            while (numeri[n] && estratti < 90);
+        }
 
-    
+    }
+
+
+
+
+
+
 }
--- Form1.cs
+++ Form1.cs
@@ -8,26 +8,41 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using tombola;
+using Pazienti_biglietto;
 
 namespace pazienti
 {
     public partial class Form1 : Form
     {
+
+        
+
         public Form1()
         {
             InitializeComponent();
+
+
+
+
+            ListView list1 = LW_paziente1;
+            ListView list2 = LW_paziente2;
+            Console.WriteLine("Premere INVIO per terminare");
+            Console.ReadLine();
         }
 
-
+       
         private void btn_paziente1_Click(object sender, EventArgs e)
         {
-                
+            Pazienti paziente1 = new Pazienti();
+            Thread p1 = new Thread(paziente1.Estrattore);
+            p1.Start(LW_paziente1);
         }
 
         private void btn_paziente2_Click(object sender, EventArgs e)
         {
-
+            Pazienti paziente2 = new Pazienti();
+            Thread p2 = new Thread(paziente2.Estrattore);
+            p2.Start(LW_paziente2);
         }
     }
 }
